c24f92592c053c75f59c78ed775ec05ef110362d
Manuel Kuß <e11722667@student.tuwien.ac.at>
2025-11-13 10:21:43 +0100

    added environments to frontend; created home page and load spinner; created run scripts in package.json

--- docs/django.md
+++ docs/django.md
@@ -6,6 +6,10 @@ python manage.py makemigrations
 python manage.py migrate
 pip install django-cors-headers
 
+Create superuser:
+python manage.py createsuperuser
+- Username: admin
+- Password: admin
 
 To run the server:
 python manage.py runserver
--- frontend/src/app/services/data-service.ts
+++ frontend/src/app/services/data-service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ChartDataResponse, ChartSeries } from '../models/chartData.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private apiUrl = 'http://localhost:8000/api/chart-data/';
+  private apiUrl = environment.apiUrl + 'chart-data/';
 
   constructor(private http: HttpClient) {}
 
